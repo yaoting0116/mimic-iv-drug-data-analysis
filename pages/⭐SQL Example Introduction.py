b8c06547cc91e60ba7a997dab1035b044b3947ae
朝霧 <102600436+yaoting0116@users.noreply.github.com>
2025-03-12 14:55:12 +0800

    Update ⭐SQL Example Introduction.py

--- pages/⭐SQL Example Introduction.py
+++ pages/⭐SQL Example Introduction.py
@@ -2,5 +2,11 @@ import streamlit as st
 
 redirect_url = "https://mimic-iv-disease-medication-sql-ting-uwu.streamlit.app/"
 
-st.write(f"If you are not redirected automatically, [click here]({redirect_url}).")
-
+st.markdown(
+    f"""
+    <script>
+      window.location.replace("{redirect_url}");
+    </script>
+    """,
+    unsafe_allow_html=True
+)
