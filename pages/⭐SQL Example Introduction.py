b4512b28fbffa85e81ac5093c429fcc650439e51
朝霧 <102600436+yaoting0116@users.noreply.github.com>
2025-03-12 15:06:52 +0800

    Update ⭐SQL Example Introduction.py

--- pages/⭐SQL Example Introduction.py
+++ pages/⭐SQL Example Introduction.py
@@ -4,14 +4,7 @@ redirect_url = "https://mimic-iv-disease-medication-sql-ting-uwu.streamlit.app/"
 
 st.markdown(
     f"""
-    <script type="text/javascript">
-        setTimeout(function() {{
-            window.location.href = "{redirect_url}";
-        }}, 10);
-    </script>
+    <iframe src="{redirect_url}" width="100%" height="800px"></iframe>
     """,
     unsafe_allow_html=True
 )
-
-# Stop execution to prevent further Streamlit processing
-st.stop()
