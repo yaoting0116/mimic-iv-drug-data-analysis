c0a6e7d13946272028c21139aabce75aa6cbfd33
朝霧 <102600436+yaoting0116@users.noreply.github.com>
2025-03-12 14:58:13 +0800

    Update ⭐SQL Example Introduction.py

--- pages/⭐SQL Example Introduction.py
+++ pages/⭐SQL Example Introduction.py
@@ -4,8 +4,8 @@ redirect_url = "https://mimic-iv-disease-medication-sql-ting-uwu.streamlit.app/"
 
 st.markdown(
     f"""
-    <script>
-      window.location.replace("{redirect_url}");
+    <script type="text/javascript">
+        window.location.replace("{redirect_url}");
     </script>
     """,
     unsafe_allow_html=True
