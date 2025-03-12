32c782cdc3f292cc52d12612101e103dfaf0ac44
朝霧 <102600436+yaoting0116@users.noreply.github.com>
2025-03-12 14:52:30 +0800

    Update ⭐SQL Example Introduction.py

--- pages/⭐SQL Example Introduction.py
+++ pages/⭐SQL Example Introduction.py
@@ -1,16 +1,6 @@
 import streamlit as st
 
-# Specify the URL you want to redirect to
 redirect_url = "https://mimic-iv-disease-medication-sql-ting-uwu.streamlit.app/"
 
-# Use an HTML meta tag to perform the redirect
-st.markdown(
-    f"""
-    <script>
-      window.location.href = "{redirect_url}";
-    </script>
-    """,
-    unsafe_allow_html=True
-)
+st.write(f"If you are not redirected automatically, [click here]({redirect_url}).")
 
-st.write("Redirecting to the new website...")
