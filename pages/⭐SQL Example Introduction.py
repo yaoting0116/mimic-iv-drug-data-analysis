877be4d3d55123b6d29a479b3127ef7f7d43bfa4
朝霧 <102600436+yaoting0116@users.noreply.github.com>
2025-03-12 15:12:49 +0800

    Update ⭐SQL Example Introduction.py

--- pages/⭐SQL Example Introduction.py
+++ pages/⭐SQL Example Introduction.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
-redirect_url = "https://mimic-iv-disease-medication-sql-ting-uwu.streamlit.app/"
+redirect_url = "https://mimic-iv-disease-medication-sql-ting-uwu.streamlit.app/?embedded=true"
 
 st.markdown(
     f"""
-    <iframe src="{redirect_url}" width="100%" height="800px"></iframe>
+    <meta http-equiv="refresh" content="0; URL={redirect_url}" />
     """,
     unsafe_allow_html=True
 )
+
+st.write("Redirecting to the new website...")
